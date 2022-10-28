c503d33c8bc866692f07d7d8151b4c9b0f13c9e6
chencheng liang <lcckkkhaha@163.com>
2022-10-28 13:37:33 +0200

    add cactus

--- src/collect_results/collect_solvability.py
+++ src/collect_results/collect_solvability.py
@@ -1,44 +1,76 @@
 import os.path
 import json
 from utils import read_solvability_JSON
-from src.utils import get_file_list,make_dirct
+from src.utils import get_file_list, make_dirct
+import pandas as pd
+from statistics import mean
+from src.plots import plot_cactus
 
 
 def main():
     folder = "/home/cheli243/PycharmProjects/Relational-Hypergraph-Neural-Network-PyG/benchmarks/test/train_data"
-    #get abstract_option_list
-    abstract_option_list= get_abstract_option_list()
+    # get abstract_option_list
+    abstract_option_list = get_abstract_option_list()
 
-    #build solvability_summary_by_abstract_option
-    solvability_summary_by_abstract_option =  get_solvability_summary_by_abstract_option(abstract_option_list,folder)
+    # build solvability_summary_by_abstract_option
+    solvability_summary_by_abstract_option = get_solvability_summary_by_abstract_option(abstract_option_list, folder)
 
-    write_solvability_summary_to_file(folder, solvability_summary_by_abstract_option, key_word="solvable_number")
+    write_solvability_summary_to_file(folder, solvability_summary_by_abstract_option, key_word="")
 
-    #todo cactus plot
-    
-    #todo write to spreadsheet
-    #todo write to latex
+    plot_cactus(get_sumary_folder(folder), solvability_summary_by_abstract_option)
+
+    # todo write to latex
 
     print("abstract_option_list", len(solvability_summary_by_abstract_option))
     for x in solvability_summary_by_abstract_option:
         print(x, solvability_summary_by_abstract_option[x])
 
 
-def write_solvability_summary_to_file(folder,solvability_summary,key_word=""):
-    output= {}
+def get_sumary_folder(folder):
     summary_folder = os.path.dirname(folder) + "/" + os.path.basename(folder) + "_summary"
     make_dirct(summary_folder)
+    return summary_folder
+
+
+def write_solvability_summary_to_file(folder, solvability_summary, key_word=""):
+    # write to json
+    output = {}
+    summary_folder = get_sumary_folder(folder)
     summary_json_file_name = summary_folder + "/solvability_summary.JSON"
     for f in solvability_summary:
         sub_fields = {}
         for k in solvability_summary[f]:
             if key_word in k:
-                sub_fields.update({k:solvability_summary[f][k]})
-        output.update({f:sub_fields})
+                sub_fields.update({k: solvability_summary[f][k]})
+        output.update({f: sub_fields})
     with open(summary_json_file_name, 'w') as f:
         json.dump(output, f, indent=4, sort_keys=True)
 
-def get_solvability_summary_by_abstract_option(abstract_option_list,folder):
+    # write to spreadsheet
+    option_list = []
+    solvable_number_list = []
+    unique_solved_number_list = []
+    safe_number_list = []
+    unsafe_number_list = []
+    average_solving_time_list = []
+    for f in solvability_summary:
+        option_list.append(f)
+        solvable_number_list.append(solvability_summary[f]["solvable_number"])
+        unique_solved_number_list.append(solvability_summary[f]["unique_solvable_number"])
+        safe_number = sum(solvability_summary[f]["satisfiability_list"])
+        safe_number_list.append(safe_number)
+        unsafe_number_list.append(len(solvability_summary[f]["satisfiability_list"]) - safe_number)
+        average_solving_time_list.append(mean(solvability_summary[f]["solvingTime_list"]))
+
+    df = pd.DataFrame(
+        dict(Options=option_list, Solvable_number=solvable_number_list,
+             Average_solving_time_ms=average_solving_time_list,
+             Unique_solved_number=unique_solved_number_list, Safe_number=safe_number_list,
+             Unsafe_number=unsafe_number_list))
+    df.to_excel(summary_folder + "/solvability_summary.xlsx")
+
+
+def get_solvability_summary_by_abstract_option(abstract_option_list, folder):
     file_list = get_file_list(folder, "smt2")
     solvability_object_list = read_solvability_JSON(file_list)
 
@@ -61,7 +93,7 @@ def get_solvability_summary_by_abstract_option(abstract_option_list,folder):
                 for m in measurement_list:
                     solvability_summary_by_abstract_option[op][m + "_list"].append(measurement_dict[m])
 
-        #todo check this implementation when have the whole dataset
+        # todo check this implementation when have the whole dataset
         # get unique solved list by differernt abstract option
         for i in solvability_summary_by_abstract_option:
             solvable_set_from_other_option = []
@@ -78,6 +110,7 @@ def get_solvability_summary_by_abstract_option(abstract_option_list,folder):
                 solvability_summary_by_abstract_option[i]["unique_solvable_list"] = list(unique_solved_list)
 
     return solvability_summary_by_abstract_option
+
 
 def get_abstract_option_list():
     abstract_option_list = []
@@ -119,6 +152,7 @@ def get_abstract_option_list():
                                     ao + "_" + g + "_" + cb + "_" + str(e) + "_" + sc + "_" + c)
 
     return abstract_option_list
+
 
 if __name__ == '__main__':
     main()
--- src/plots.py
+++ src/plots.py
@@ -5,8 +5,12 @@ import mlflow
 import plotly.graph_objects as go
 from sklearn.metrics import confusion_matrix
 import plotly.express as px
+import numpy as np
+
 plt.style.use("ggplot")
-def loss_plot(train_loss_floats,valid_loss_floats):
+
+
+def loss_plot(train_loss_floats, valid_loss_floats):
     fig = go.Figure()
     fig.add_trace(go.Scatter(
         x=list(range(len(train_loss_floats))),
@@ -25,34 +29,83 @@ def loss_plot(train_loss_floats,valid_loss_floats):
         yaxis_title='loss')
     plot_name = "loss"
     save_file_name = "../figures/" + plot_name + ".html"
-    #fig.update_yaxes(type="linear")
+    # fig.update_yaxes(type="linear")
     fig.write_html(save_file_name)
     mlflow.log_artifact(save_file_name)
 
 
-def draw_label_pie_chart(num_label,learning_label,name=""):
+def draw_label_pie_chart(num_label, learning_label, name=""):
     label_name_list = list(range(num_label))
     flat_list = [item for sublist in learning_label for item in sublist]
     values = [flat_list.count(i) for i in label_name_list]
-    pull = [0.2 if v/sum(values)<0.01 else 0 for v in values] # if percentage < 0.01, pull it out from the pie
-    fig = go.Figure(data=[go.Pie(labels=label_name_list, values=values,pull=pull)])
+    pull = [0.2 if v / sum(values) < 0.01 else 0 for v in values]  # if percentage < 0.01, pull it out from the pie
+    fig = go.Figure(data=[go.Pie(labels=label_name_list, values=values, pull=pull)])
 
-    save_file_name = "../figures/"+name+"-distribution.html"
+    save_file_name = "../figures/" + name + "-distribution.html"
     fig.write_html(save_file_name)
     mlflow.log_artifact(save_file_name)
     return values
 
 
-def draw_confusion_matrix(predicted_y,true_y,num_classes,name=""):
-    label_list=list(range(num_classes))
-    cm=confusion_matrix(true_y,predicted_y,labels=label_list)
-    fig = px.imshow(cm,x=label_list,y=label_list, text_auto=True)
+def draw_confusion_matrix(predicted_y, true_y, num_classes, name=""):
+    label_list = list(range(num_classes))
+    cm = confusion_matrix(true_y, predicted_y, labels=label_list)
+    fig = px.imshow(cm, x=label_list, y=label_list, text_auto=True)
     fig.update_layout(
         title='confusion matrix',
         xaxis_title='predicted_y',
         yaxis_title='true_y')
-    save_file_name = "../figures/"+name+"-confusion-matrix.html"
+    save_file_name = "../figures/" + name + "-confusion-matrix.html"
     fig.write_html(save_file_name)
     mlflow.log_artifact(save_file_name)
 
 
+def plot_cactus(summary_folder, solvability_summary):
+    max_time = np.max(np.max(np.array([item["solvingTime_list"] for item in solvability_summary.values()]))) / 1000
+    # print("max_time s",max_time)
+    cactus = {}
+    time_limit = int(max_time) + 1
+    for option in solvability_summary:
+        cactus[option] = [0]
+        solved_index = 0
+        for t in range(0, time_limit):
+            solved_counter = 0
+            for st in solvability_summary[option]["solvingTime_list"]:
+                if st / 1000 < t:
+                    solved_counter = solved_counter + 1
+                if solved_counter > solved_index:
+                    cactus[option].append(t)
+                    solved_index = solved_counter
+
+    # key_words=["Term","Octagon","RelationalEqs","RelationalIneqs"]
+    # for k in key_words:
+    #     draw_one_cactus(summary_folder,cactus,k)
+
+    # draw_one_cactus(summary_folder, cactus, "")
+    draw_one_cactus_plotly(summary_folder, cactus, key_word="", scale="linear")
+    draw_one_cactus_plotly(summary_folder, cactus, key_word="", scale="log")
+
+
+def draw_one_cactus_plotly(summary_folder, cactus, key_word, scale=""):
+    # sort lines by solved problems for the highest time limit
+    lines = []
+    for k in cactus:
+        if key_word in k:
+            lines.append((len(cactus[k]), k, cactus[k]))
+    lines.sort(reverse=True)
+
+    fig = go.Figure()
+    for line in lines:
+        if key_word in line[1]:
+            fig.add_trace(go.Scatter(
+                x=list(range(len(line[2]))),
+                y=line[2],
+                name=line[1]
+            ))
+
+    fig.update_layout(  # legend_title_text='Trend',
+        title='',
+        xaxis_title='solved benchmarks',
+        yaxis_title='time limit (s)')
+    fig.update_yaxes(type=scale)
+    fig.write_html(summary_folder + "/" + key_word + "-" + scale + "-cactus.html")
