f7fe659c83333d67ba48a5d5f7112988b0982fca
chencheng liang <lcckkkhaha@163.com>
2022-10-28 12:03:29 +0200

    write solvability summary to json file

--- src/collect_results/collect_solvability.py
+++ src/collect_results/collect_solvability.py
@@ -1,41 +1,83 @@
 import os.path
-
+import json
 from utils import read_solvability_JSON
-from src.utils import get_file_list
+from src.utils import get_file_list,make_dirct
 
 
 def main():
     folder = "/home/cheli243/PycharmProjects/Relational-Hypergraph-Neural-Network-PyG/benchmarks/test/train_data"
-    file_list = get_file_list(folder, "smt2")
-    solvability_object_list = read_solvability_JSON(file_list)
-
     #get abstract_option_list
     abstract_option_list= get_abstract_option_list()
 
-    measurement_list = ["solvingTime", "cegarIterationNumber", "predicateGeneratorTime", "satisfiability"]
-    # change getSolvability and CEGAR in Eldarica to collect generatedPredicateNumber and averagePredicateSize
-
-
     #build solvability_summary_by_abstract_option
-    solvability_summary_by_abstract_option={op:dict({m+"_list":[] for m in measurement_list}, **{"solvable_number": 0, "solvable_list": [],"unique_solvable_number": 0, "unique_solvable_list": []}) for op in abstract_option_list }
+    solvability_summary_by_abstract_option =  get_solvability_summary_by_abstract_option(abstract_option_list,folder)
 
+    write_solvability_summary_to_file(folder, solvability_summary_by_abstract_option, key_word="solvable_number")
 
-    # fill in solvability_summary_by_abstract_option by iterating the solvability list
-    for solvability_object in solvability_object_list:
-        for op in solvability_summary_by_abstract_option:
-            measurement_dict={m:int(solvability_object[m+"_"+op][0]) for m in measurement_list}
-            if measurement_dict["solvingTime"]!=10800000:
-                solvability_summary_by_abstract_option[op]["solvable_number"] += 1
-                solvability_summary_by_abstract_option[op]["solvable_list"].append(os.path.basename(solvability_object["file_name"]))
-                for m in measurement_list:
-                    solvability_summary_by_abstract_option[op][m+"_list"].append(measurement_dict[m])
-
+    #todo cactus plot
+    
+    #todo write to spreadsheet
+    #todo write to latex
 
     print("abstract_option_list", len(solvability_summary_by_abstract_option))
     for x in solvability_summary_by_abstract_option:
         print(x, solvability_summary_by_abstract_option[x])
 
 
+def write_solvability_summary_to_file(folder,solvability_summary,key_word=""):
+    output= {}
+    summary_folder = os.path.dirname(folder) + "/" + os.path.basename(folder) + "_summary"
+    make_dirct(summary_folder)
+    summary_json_file_name = summary_folder + "/solvability_summary.JSON"
+    for f in solvability_summary:
+        sub_fields = {}
+        for k in solvability_summary[f]:
+            if key_word in k:
+                sub_fields.update({k:solvability_summary[f][k]})
+        output.update({f:sub_fields})
+    with open(summary_json_file_name, 'w') as f:
+        json.dump(output, f, indent=4, sort_keys=True)
+
+def get_solvability_summary_by_abstract_option(abstract_option_list,folder):
+    file_list = get_file_list(folder, "smt2")
+    solvability_object_list = read_solvability_JSON(file_list)
+
+    measurement_list = ["solvingTime", "cegarIterationNumber", "predicateGeneratorTime", "satisfiability"]
+    # change getSolvability and CEGAR in Eldarica to collect generatedPredicateNumber and averagePredicateSize
+
+    solvability_summary_by_abstract_option = {op: dict({m + "_list": [] for m in measurement_list},
+                                                       **{"solvable_number": 0, "solvable_list": [],
+                                                          "unique_solvable_number": 0, "unique_solvable_list": []}) for
+                                              op in abstract_option_list}
+
+    # fill in solvability_summary_by_abstract_option by iterating the solvability list
+    for solvability_object in solvability_object_list:
+        for op in solvability_summary_by_abstract_option:
+            measurement_dict = {m: int(solvability_object[m + "_" + op][0]) for m in measurement_list}
+            if measurement_dict["solvingTime"] != 10800000:
+                solvability_summary_by_abstract_option[op]["solvable_number"] += 1
+                solvability_summary_by_abstract_option[op]["solvable_list"].append(
+                    os.path.basename(solvability_object["file_name"]))
+                for m in measurement_list:
+                    solvability_summary_by_abstract_option[op][m + "_list"].append(measurement_dict[m])
+
+        #todo check this implementation when have the whole dataset
+        # get unique solved list by differernt abstract option
+        for i in solvability_summary_by_abstract_option:
+            solvable_set_from_other_option = []
+            for j in solvability_summary_by_abstract_option:
+                if j != i:
+                    solvable_set_from_other_option = solvable_set_from_other_option + \
+                                                     solvability_summary_by_abstract_option[j][
+                                                         "solvable_list"]
+            solvable_set_from_other_option = list(set(solvable_set_from_other_option))
+            unique_solved_list = set(solvability_summary_by_abstract_option[i]["solvable_list"]).difference(
+                set(solvable_set_from_other_option))
+            if len(unique_solved_list) != 0:
+                solvability_summary_by_abstract_option[i]["unique_solvable_number"] = len(unique_solved_list)
+                solvability_summary_by_abstract_option[i]["unique_solvable_list"] = list(unique_solved_list)
+
+    return solvability_summary_by_abstract_option
 
 def get_abstract_option_list():
     abstract_option_list = []
