ee3d9fa958fe957cfe6ebccaf63b0a93ad97c490
chencheng liang <lcckkkhaha@163.com>
2022-11-29 17:48:32 +0100

    collect unsat core task results

---
+++ src/collect_results/collect_solvability_unsat_core.py
@@ -0,0 +1,20 @@
+
+from utils import read_files,read_json_file
+from src.utils import get_file_list, make_dirct
+
+
+
+
+
+
+
+def main():
+    folder=""
+    for file in get_file_list(folder,"smt2"):
+        print(file)
+        file_name=file[:-len(".zip")]
+        
+
+
+if __name__ == '__main__':
+    main()
