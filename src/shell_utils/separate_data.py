bcfc73a923da77cb5c948f14a3c5b74026c2e38e
chencheng liang <lcckkkhaha@163.com>
2023-03-17 09:44:27 +0100

    add separate data

---
+++ src/shell_utils/separate_data.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.path.append("/home/cheli243/PycharmProjects/Relational-Hypergraph-Neural-Network-PyG")
+from src.collect_results.utils import copy_relative_files
+from src.utils import get_file_list, make_dirct
+from shutil import copy
+
+
+def main():
+    folder = sys.argv[1]
+    n = int(sys.argv[2])
+    file_list = get_file_list(folder, "smt2")
+    divided_list = divide_list(file_list, n)
+
+    for i, l in enumerate(divided_list):
+        current_folder = make_dirct(folder + "-part-" + str(i))
+        for f in l:
+            copy(f, current_folder)
+
+
+def divide_list(lst, n):
+    """Divide a list into n roughly equal parts."""
+    division = len(lst) / n
+    return [lst[round(division * i):round(division * (i + 1))] for i in range(n)]
+
+
+if __name__ == '__main__':
+    main()
